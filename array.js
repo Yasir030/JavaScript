9f9441b6c34b3dcc92d1aa6e0f63d86c0315dae8
Yasir030 <mdyasirarafat030@gmail.com>
2023-10-16 12:27:32 +0600

    basic array

---
+++ array.js
@@ -0,0 +1,12 @@
+const fruits = ["apple","Orange", "Mango", "Banana"];
+
+
+fruits[0]= "grape";
+fruits[4]="pineapple";
+
+console.log(fruits)
+console.log(fruits[fruits.length -1])  //priting last index of array
+
+// Loop
+
+const car = ["toyta", "nissan", "tata", "mahindra"]
